5e9f392fcc89d38a061e094cc4b916f6903f2ec8
igor04 <igor.s04@mail.ru>
2012-07-27 23:32:09 +0300

    Fix for horoku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	require 'openid/store/filesystem'
-  provider :twitter, 'vseSy46UauC7AzQykh7NzQ', 'PYx8fk4kT6fXaXr0SqgiatJeMR5X3MrHTtkUc2p8'
+  provider :twitter, 'hlTjUFNSkXI0daNISkWBTQ', 'BwTcwrUyxhkqLFCASR9C6OBdtmOpql3TnqfxlCsyY'
   provider :open_id, :store => OpenID::Store::Filesystem.new('/tmp'), :identifier => 'https://www.google.com/accounts/o8/id'
 end
